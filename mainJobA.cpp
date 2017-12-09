5239bef831edf020607026592132e4ca60d72457
Caitlin <31714143+cabaker3@users.noreply.github.com>
2017-12-09 14:36:52 -0500

    Update mainJobA.cpp

--- mainJobA.cpp
+++ mainJobA.cpp
@@ -6,3 +6,15 @@
 //time integration of a deforming elastic body
   //done in FEA using numerical integration
   //implicit integration needs a Jacobian J
+
+int max_itr = 10;
+
+for (int itr =0; itr < max_itr ; itr ++)
+{
+  // compute the update for the assigned x[i]
+#pragma omp barrier
+{
+  // write x[i] in global memory
+}
+#pragma omp barrier
+}
