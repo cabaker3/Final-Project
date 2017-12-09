80062f6a6675f2473e43a225ecf4cc9f650346f8
Caitlin <31714143+cabaker3@users.noreply.github.com>
2017-12-09 10:43:48 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,6 +5,14 @@
 
 int main(int argc, char *argv[]) {
 	
-  int N = atoi(argv[1]);
+	int N = atoi(argv[1]);
+	
+	omp_set_num_threads(N);
+
+	double start = omp_get_wtime();
+	
+	double stop = omp_get_wtime();
+	
+	double time = (stop - start) * 1000;
   
 }
