918adcba2d449ab65839c9c8c0b2dcf9855f1bd8
Caitlin <31714143+cabaker3@users.noreply.github.com>
2017-12-09 14:37:40 -0500

    Update mainJobA.cpp

--- mainJobA.cpp
+++ mainJobA.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "omp.h"
 #include <fstream>
+#include "helperJobB.cpp"
 
 //run on the host using OpenMP
 //time integration of a deforming elastic body
@@ -12,6 +13,7 @@ int max_itr = 10;
 for (int itr =0; itr < max_itr ; itr ++)
 {
   // compute the update for the assigned x[i]
+  // call helperJobB
 #pragma omp barrier
 {
   // write x[i] in global memory
