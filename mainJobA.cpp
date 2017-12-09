052a7a2c6a21a32623927ed6351eca86a82de391
Caitlin <31714143+cabaker3@users.noreply.github.com>
2017-12-09 13:35:07 -0500

    Update mainJobA.cpp

--- mainJobA.cpp
+++ mainJobA.cpp
@@ -1,3 +1,8 @@
 #include <iostream>
 #include "omp.h"
 #include <fstream>
+
+//run on the host using OpenMP
+//time integration of a deforming elastic body
+  //done in FEA using numerical integration
+  //implicit integration needs a Jacobian J
