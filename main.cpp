6f3268635b9506d8f8f03f0f14701d8dcc5e001b
Caitlin <31714143+cabaker3@users.noreply.github.com>
2017-12-09 13:32:57 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
 #include <omp.h>
 #include <fstream>
+#define OMPI_SKIP_MPICXX  /* Don't use OpenMPI's C++ bindings (they are deprecated) */
+#include <mpi.h>
 
 int main(int argc, char *argv[]) {
+	//software backbone/infrastructure that allows mainJobA to carry on while helperJobB works asynchronously to update
+	//the computationally costly ingredient needed by mainJobA
+	
+	//mainJobA runs on one node, helperJobB runs on another node; updates via MPI
 	
 	int N = atoi(argv[1]);
 	
