e19b33a7913c10327a29416fdc0652cbc87a6dbd
Caitlin <31714143+cabaker3@users.noreply.github.com>
2017-12-13 15:03:48 -0500

    Update mainJobA.cpp

--- mainJobA.cpp
+++ mainJobA.cpp
@@ -2,13 +2,24 @@
 #include "omp.h"
 #include <fstream>
 #include "helperJobB.cpp"
+#include <math.h>
 
 //run on the host using OpenMP
 //time integration of a deforming elastic body
   //done in FEA using numerical integration
   //implicit integration needs a Jacobian J
-
-int max_itr = 10;
+float mainJobA(n_t,n_x,a,L,tmax,error_plots){
+  float dx = L/(n_x - 1);
+  float dt = tmax/(n_t - 1);
+  float r = alpha * dt / pow(dx,2);
+  float r2 = 1 - 2*r;
+  
+  //create arrays to save data for export
+  //x = ... L,n_x
+  //t = ... tmax,n_t
+  //U = ... n_x,n_t
+}
+/*int max_itr = 10;
 
 for (int itr =0; itr < max_itr ; itr ++)
 {
@@ -19,4 +30,4 @@ for (int itr =0; itr < max_itr ; itr ++)
   // write x[i] in global memory
 }
 #pragma omp barrier
-}
+}*/
