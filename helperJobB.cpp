76fef3b11cc67cbdbc6fb4646701dee936d3222b
Caitlin <31714143+cabaker3@users.noreply.github.com>
2017-12-13 15:28:07 -0500

    Update helperJobB.cpp

--- helperJobB.cpp
+++ helperJobB.cpp
@@ -9,8 +9,8 @@
   //update might be slow to the point where only some percent of J entries are update
     //then should keep updating the remaining stale entries of J over the next time step
 
-float helperJobB(float U,int n_t, int n_x, float r, float r2){
-  for(int m = 2; m <= n_t; m++){
+float helperJobB(float U,int nt, int n_x, float r, float r2){
+  for(int m = 2; m <= nt; m++){
     for(int i = 2; i <= (n_x - 1); i++){
       U[i][m] = r*U[i-1][m-1] + r2*U[i][m-1] + r*U[i+1][m-1]; 
     }
