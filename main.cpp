bc6cdae829fd1fcb470871b6c21d8f7a975f381a
Caitlin <31714143+cabaker3@users.noreply.github.com>
2017-12-15 12:22:47 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,8 +10,8 @@ int main(int argc, char *argv[]) {
 	//software backbone/infrastructure that allows mainJobA to carry on while helperJobB works asynchronously to update
 	//the computationally costly ingredient needed by mainJobA
 	//mainJobA runs on one node, helperJobB runs on another node; updates via MPI
-	int nt, n_x, L, errPlots;
-	float a, tmax;
+	int L;
+	float g, dx, dt, IM;
 	if (argc != 2) {
 		std::cerr << "Usage: " << argv[0] << " N\n";
 		return -1;
@@ -21,33 +21,33 @@ int main(int argc, char *argv[]) {
 	omp_set_num_threads(num_threads);
 	
 	if (argc != 3) {
-		nt = 10;
+		L = 1;
 	}else{
-		nt = atoi(argv[2]);
+		L = atoi(argv[2]);
 	}
 	
 	if (argc != 4) {
-		n_x = 20;
+		g = 1.4;
 	}else{
-		n_x = atoi(argv[3]);
+		g = atoi(argv[3]);
 	}
 	
 	if (argc != 5) {
-		a = 0.1;
+		dx = 0.01;
 	}else{
 		a = atoi(argv[4]);
 	}
 	
 	if (argc != 6) {
-		L = 1;
+		dt = 1/2000;
 	}else{
-		L = atoi(argv[5]);
+		dt = atoi(argv[5]);
 	}
 	
 	if (argc != 7) {
-		tmax = 0.5;
+		IM = L/dx;
 	}else{
-		tmax = atoi(argv[6]);
+		L = atoi(argv[6]);
 	}
 	
 	errPlots = 1;
@@ -57,7 +57,7 @@ int main(int argc, char *argv[]) {
 	//create array to store result
 	
 	#pragma omp parallel shared (nt,n_x,a,L,tmax,errPlots)
-		mainJobA(nt,n_x,a,L,tmax,errPlots); //save to array
+		//mainJobA(nt,n_x,a,L,tmax,errPlots); //save to array
 	
 	double stop = omp_get_wtime();
 	
