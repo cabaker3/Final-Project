bcd58a7507907d8f3f627c80d457968bf7577c23
Caitlin <31714143+cabaker3@users.noreply.github.com>
2017-12-15 19:08:09 -0500

    Update mainJobA_godunov.cpp

--- mainJobA_godunov.cpp
+++ mainJobA_godunov.cpp
@@ -12,13 +12,14 @@ float mainJobA(int L, float g, dx, dt, IM){
   int i = 1;
   
   //create 2D array Qi
+  auto Qi = new float [][];
   
   //Lax Initial Conditions
   for(int x = 0; x <= 1; x+=dx){
     if(x <= 0.5){
-      //Qi[i] = [0.445; 0.311; 8.928];
+      Qi[1][i] = {0.445; 0.311; 8.928};
     }else{
-      //Qi[i] = [0.5; 0; 1.4275];  
+      Qi[1][i] = {0.5; 0; 1.4275};  
     }
     i += 1; //change to for loop
   }
@@ -55,9 +56,48 @@ float mainJobA(int L, float g, dx, dt, IM){
   
   int k = 1;
   
+  //initialize rho,u,et,p
+  
   for(int t = 0; t <= 0.16; t+=dt){
     //call helperJobB
     helperJobB(); //Flux & Qnew
+    
+    Qn1[][1] = Qi[][1];
+    Qn1[][101] = Qi[][101];
+    Qnew = Qn1;
+    Qold = Qnew;
+    
+    for(int i = 1; i < IM+1; i++){
+    //Density
+    rho[i][] = Qnew[1][i];
+    
+    //Velocity
+    u[i] = Qnew[2][i] / rho[i][];
+    
+    //Total Energy
+    et[i][] = Qnew[3][i] / rho[i][];
+    
+    //Pressure, from the equation of state
+    p[i][] = ();
+      
+    //Speed of Sound
+    a[i][] = sqrt(g*p[i][]/rho[i][]);
+    
+    //Intial E Matrix
+    E[][i] = {rho[i][]*u[i][], rho[i][]*pow(ui[i][],2) + p[i][], et[i][]*rho[i][]*u[i][]+p[i][]*u[i][]};
+    
+    //Eigenvalues
+    eigen[][i] = {u[i][], u[i][] + a[i][], u[i][]-a[i][]};
+  }
+    //Alpha
+    alpha = max(abs(eigen));
+    
+    um[][k] =u[][1];
+    rhom[][k] = rho[][1];
+    pm[][k] = p[][1];
+    etm[][k] = et[][1];
+    
+    k += 1;
   }
 }
 /*int max_itr = 10;
