39c1a426d00cfa62261dc0329c9b0542f3fa1f6f
Caitlin <31714143+cabaker3@users.noreply.github.com>
2017-12-19 16:57:06 -0500

    Update helperJobB_kernel.cu

--- helperJobB_kernel.cu
+++ helperJobB_kernel.cu
@@ -1,1 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include <cuda.h>
+#include <iostream>
+#include <math.h>
 
+__global__ void helperJobB(float *alpha, float *E, float *Qold, float *Qnew, float *F, float IM){
+  int j = blockIdx.x * blockDim.x + threadIdx.x;
+  int i = blockIdx.y * blockDim.y + threadIdx.y;
+  
+  for(int j = 1; j <= 3; j++){
+    for(int i = 1; i <= IM; i++)
+      F[j][i] = 0.5 * (E[j][i] + E[j][i+1]) - 0.5 * abs(alpha[i][1]) * (Qold[j][i+1] - Qnew[j][i]);
+  }
+  
+}
